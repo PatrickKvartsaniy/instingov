eec67f9d1ad877908ae7449961d3e8420859a7a3
PatrickKvartsaniy <kvartsaniy@gmail.com>
2020-12-05 13:30:23 +0200

    feat: contact page

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('areas/', include('areas.urls')),
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
+    path('contact/', include('contact.urls')),
 ]
